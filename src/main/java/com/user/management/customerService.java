f303585a884e3570f27bd10ea2228306879319d2
udesh-sachin <it19958866@my.sliit.lk>
2022-04-24 12:58:53 +0530

    Delete function
    customer Remove Function implement

--- src/main/java/com/user/management/customerService.java
+++ src/main/java/com/user/management/customerService.java
@@ -56,8 +56,8 @@ public class customerService {
 		//Convert the input string to an XML document
 		Document doc = Jsoup.parse(itemData, "", Parser.xmlParser());
 		//Read the value from the element <itemID>
-		String itemID = doc.select("itemID").text();
-		String output = itemObj.deleteItem(itemID);
+		String cusID = doc.select("CusID").text();
+		String output = itemObj.deleteItem(cusID);
 		return output;
 		}
 		
