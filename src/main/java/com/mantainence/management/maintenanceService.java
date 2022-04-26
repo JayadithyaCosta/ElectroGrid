3655b78314895e03fb1d60b32ec1a460b799bcf5
HasithYoman <81187406+HasithYoman@users.noreply.github.com>
2022-04-26 23:43:31 +0530

    complete all crud anf fix errors

--- src/main/java/com/mantainence/management/maintenanceService.java
+++ src/main/java/com/mantainence/management/maintenanceService.java
@@ -1,79 +1,92 @@
 package com.mantainence.management;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import model.Maintenance;
 
-//For REST Service
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-//For JSON
-import com.google.gson.*;
-//For XML
-import org.jsoup.*;
-import org.jsoup.parser.*;
-import org.jsoup.nodes.Document;
 
-
-@Path("/Maintenance")
 public class maintenanceService {
-	Maintenance MaintenanceObj = new Maintenance();
-	@GET
-	@Path("/AddMaintenance")
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	Maintenance maintenanceObj = new Maintenance();
+	/*@GET
+	@Path("/")
 	@Produces(MediaType.TEXT_HTML)
-	public String insertNewMaintenance(
-			@FormParam("name")String name,
-			@FormParam("area")String area,
-			@FormParam("place")String place,
-			@FormParam("time")String time,
-			@FormParam("status")String status) {
-	   String output= MaintenanceObj.insertMaintenance(name, area, place, 0, status);
-			   return output;
+	public String readItems1()
+	{
+	return "Hello";
 	}
+	*/
+	//read
 	@GET
 	@Path("/")
 	@Produces(MediaType.TEXT_HTML)
-	public String getMaintenance() {
-		return MaintenanceObj.readMaintenance();
+	public String readItems()
+	{
+	return maintenanceObj.readMaintenance();
 	}
 	
-	@PUT
-	@Path("/UpdateMeintenace")
-	@Consumes(MediaType.APPLICATION_JSON)
+	//insert
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces(MediaType.TEXT_PLAIN)
-	public String UpdateMaintenace(String MaintenanceData){
-		
-		JsonObject MaintenanceObject = new JsonParser().parse(MaintenanceData).getAsJsonObject();
-		
-		String MaintenanceID = MaintenanceObject.get("MaintenanceID").getAsString();
-		String Maintenancename = MaintenanceObject.get("MaintenanceName").getAsString();
-		String MaintenanceArea = MaintenanceObject.get("MaintenanceArea").getAsString();
-		String MaintenancePlace = MaintenanceObject.get("MaintenancePlace").getAsString();
-		String MaintenanceTime = MaintenanceObject.get("MaintenanceTime").getAsString();
-		String MaintenanceStatus = MaintenanceObject.get("MaintenanceStatus").getAsString();
-		
-		String output= MaintenanceObj.updateMaintenance(MaintenanceID, Maintenancename, MaintenanceArea, MaintenancePlace, MaintenanceTime, MaintenanceStatus);
-				
-		return output;
-		
+	public String insertManagement(@FormParam("name") String name,
+	@FormParam("area") String area,
+	@FormParam("place") String place,
+	@FormParam("time") String time,
+	@FormParam("status") String status)
+	{
+	String output = maintenanceObj.insertMaintenance(name, area, place, time, status);
+	return output;
+	
+
 	}
+	//delete
 	@DELETE
 	@Path("/")
 	@Consumes(MediaType.APPLICATION_XML)
 	@Produces(MediaType.TEXT_PLAIN)
-	public String deleteMaintenance(String MaintenanceData)
+	public String deleteMaintenance(String maintenanceData)
 	{
 	//Convert the input string to an XML document
-		Document doc = Jsoup.parse(MaintenanceData, "", Parser.xmlParser());
-		//Read the value from the element <MaintenanceID>
-		String MaintenanceID = doc.select("MaintenanceID").text();
-		
-		String output = MaintenanceObj.deleteMaintenance(MaintenanceID);
-				
-		return output;
+	Document doc = Jsoup.parse(maintenanceData, "", Parser.xmlParser());
+	//Read the value from the element <itemID>
+	String maintenanceID = doc.select("maintenanceID").text();
+	String output = maintenanceObj.deleteMaintenance(maintenanceID);
+	return output;
 	}
-
-
 	
-	
-
+	//update
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateMaintenance(String maintenanceData)
+	{
+	//Convert the input string to a JSON object
+	JsonObject maintenanceObject = new JsonParser().parse(maintenanceData).getAsJsonObject();
+	//Read the values from the JSON object
+	String maintenanceID = maintenanceObject.get("maintenanceID").getAsString();
+	String name = maintenanceObject.get("name").getAsString();
+	String area = maintenanceObject.get("area").getAsString();
+	String place = maintenanceObject.get("place").getAsString();
+	String time = maintenanceObject.get("time").getAsString();
+	String status = maintenanceObject.get("status").getAsString();
+	String output = maintenanceObj.updateMaintenance(maintenanceID, name, area, place, time,status);
+	return output;
+	}
 }
--- src/main/java/model/Maintenance.java
+++ src/main/java/model/Maintenance.java
@@ -1,187 +1,201 @@
 package model;
-import java.sql.*;
 
-public class Maintenance{
-	private Connection connect() {
-		Connection con=null;
-		
-		try
-		{
-			Class.forName("com.mysql.jdbc.Driver");
-			
-			con=DriverManager.getConnection("");
-		}
-		catch(Exception e )
-		{e.printStackTrace();}
-		
-		return con;
-		
-	}
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Maintenance {
 	
-	public String insertMaintenance(String name, String area, String place, double time, String status) 
+	
+	private static String url = "jdbc:mysql://localhost:3306/eg";
+	private static String userName = "root";
+	private static String password = "12Hasith3";
+	
+	
+	public Connection connect()
 	{
-		
-		String output="";
-		
-		try 
-		{
-			Connection con=connect();
-			
-			if(con ==null)
-			{return "Error while connecting to the inserting.";}
-			
-			String query ="insert in to Maintenance('MaintenanceID','name','area','place', 'time','satus')"
-					+"value(?,?,?,?,?,?)";
-			
-			PreparedStatement preparedStmt= con.prepareStatement(query);
-			
-			//binding values
-			preparedStmt.setInt(1,0);
-			preparedStmt.setString(2,name);
-			preparedStmt.setString(3,area);
-			preparedStmt.setString(4,place);
-			preparedStmt.setDouble(5,time);
-			preparedStmt.setString(5,status);
-			
-			//execute the statement
-			
-			preparedStmt.execute();
-			
-			output="inserted successfully";
-				
-		}
-		catch(Exception e) {
-			output="error while inserting the Maitenace request";
-			System.err.println(e.getMessage());
-		}
-		return output;
-	}
-	public String readMaintenance() 
+	Connection con = null;
+	
+	try
 	{
-		String output="";
-		
-			try
-			{
-				Connection con=connect();
-			
-				if(con== null)
-				{return "Error while connecting to the database for reading."; }
-			
-				// Prepare the html table to be displayed
-				output = "<table border='1'><tr><th>Maintenance ID</th><th>Item Name</th>" +
-						"<th>name</th>" +
-						"<th>Area</th>" +
-						"<th>Place</th>" +
-						"<th>Time</th>" +
-						"<th>Status</th>" +
-						"<th>Update</th><th>Remove</th></tr>";
-				String query = "select * from items";
-				Statement stmt = con.createStatement();
-				ResultSet rs = stmt.executeQuery(query);
-			
-			
-		
-				while(rs.next()) {
-					String MaintenanceID=Integer.toString(rs.getInt("MaintenanceID"));
-					String name=rs.getString("name");
-					String area=rs.getString("area");
-					String place=rs.getString("place");
-					Double time=rs.getDouble("time");
-					String status=rs.getString("status");
-					
-					
-					// Add into the html table
-					output += "<tr><td>" + name + "</td>";
-					output += "<td>" + area + "</td>";
-					output += "<td>" + place + "</td>";
-					output += "<td>" + time + "</td>";
-					output += "<td>" + status + "</td>";
-					
-		
-				//buttons
-				output = "<td><input name='btnpdate' type ='button' value='Update "
-					+ "class='btn-secndary'></td>"
-					+"<td><form method='post' action='items.jsp'>"
-					+"<input name='itemID' type='hidden value'"+ MaintenanceID
-					+"'>"+"</form></td></tr>";
-							
-				}
-				con.close();
-		//cmplete the html table
-					output += "</table>";
+	  Class.forName("com.mysql.jdbc.Driver");
+	  con= DriverManager.getConnection(url,userName,password);
+	  //For testing
+	  System.out.print("Successfully connected");
 	}
 	catch(Exception e)
 	{
-		output = "Error while reading the Maintenance.";
-		System.err.println(e.getMessage());
-			
-	}
-	return output;
+		System.out.println("Database connection is not success!!!");
 	}
 	
-	public String updateMaintenance(String MaintenanceID, String name, String area, String place, String time, String status) 
+	return con;
+	}
+	
+	
+	//insert method
+		public String insertMaintenance(String name, String area, String place, String time, String status)
+		{
+		   String output = "";
+		  try
+		   {
+		      Connection con = connect();
+		      if (con == null)
+		   {
+		    return "Error while connecting to the database";
+		   }
+		   // create a prepared statement
+		   String query = "insert into maintenance (maintenanceID,name,area,place,time,status) values (?,?,?,?,?,?)";
+		   
+		   
+		   PreparedStatement preparedStmt = con.prepareStatement(query);
+		   // binding values
+		   preparedStmt.setInt(1, 0);
+		   preparedStmt.setString(2, name);
+		   preparedStmt.setString(3, area);
+		   preparedStmt.setString(4, place);
+		   preparedStmt.setString(5, time);
+		   preparedStmt.setString(6, status);
+		
+		
+		  //execute the statement
+		  preparedStmt.execute();
+		  con.close();
+		  output = "Inserted successfully";
+		  }
+		  catch (Exception e)
+		  {
+		  output = "Error while inserting";
+		  System.err.println(e.getMessage());
+		  }
+		  return output;
+		  }
+		
+	
+	//read
+	
+	public String readMaintenance()
+	  {
+	     String output = "";
+	     try
+	    {
+	      Connection con = connect();
+	      if (con == null)
+	    {
+	        return "Error while connecting to the database for reading.";
+	    }
+	// Prepare the html table to be displayed
+	   output = "<table border='1'><tr><th>name</th>"
+	            +"<th>area</th><th>place</th>"
+	            + "<th>time</th>"
+	            + "<th>status</th>"
+	            + "<th>Update</th><th>Remove</th></tr>";
+	   
+	    String query = "select * from maintenance";
+	    Statement stmt = con.createStatement();
+	    ResultSet rs = stmt.executeQuery(query);
+	// iterate through the rows in the result set
+	   while (rs.next())
+	   {
+	     String maintenanceID = Integer.toString(rs.getInt("maintenanceID"));
+	     String name = rs.getString("name");
+	     String area = rs.getString("area");
+	     String place = rs.getString("place");
+	     String time = rs.getString("time");
+	     String status = rs.getString("status");
+	// Add a row into the html table
+	    output += "<tr><td>" + name + "</td>";
+	    output += "<td>" + area + "</td>";
+	    output += "<td>" + place + "</td>";
+	
+	    output += "<td>" + time + "</td>";
+	    
+	    output += "<td>" + status + "</td>";
+	// buttons
+	    output += "<td><input name='btnUpdate' "
+	    + " type='button' value='Update'></td>"
+	    + "<td><form method='post' action='items.jsp'>"
+	    + "<input name='btnRemove' "
+	    + " type='submit' value='Remove'>"
+	    + "<input name='maintenanceID' type='hidden' "
+	    + " value='" + maintenanceID + "'>" + "</form></td></tr>";
+	    }
+	    con.close();
+	// Complete the html table
+	    output += "</table>";
+	    }
+	   catch (Exception e)
+	  {
+	      output = "Error while reading the maintenance.";
+	      System.err.println(e.getMessage());
+	  }
+	    return output;
+	 }
+	
+	 //delete
+	
+	public String deleteMaintenance(String maintenanceID)
+	 {
+	   String output = "";
+	 try
+	 {
+	    Connection con = connect();
+	    if (con == null)
+	  {
+	       return "Error while connecting to the database for deleting.";
+	   }
+	// create a prepared statement
+	      String query = "delete from maintenance where maintenanceID=?";
+	      PreparedStatement preparedStmt = con.prepareStatement(query);
+	// binding values
+	      preparedStmt.setInt(1, Integer.parseInt(maintenanceID));
+	// execute the statement
+	      preparedStmt.execute();
+	      con.close();
+	      output = "Deleted successfully";
+	 }
+	 catch (Exception e)
+	 {
+	      output = "Error while deleting the item.";
+	      System.err.println(e.getMessage());
+	 }
+	 return output;
+	 }
+	
+	//update
+	public String updateMaintenance(String maintenanceID, String name, String area, String place, String time,String status)
+	
 	{
+	    String output = "";
+	try
+	{
+	   Connection con = connect();
+	   if (con == null)
+	    {  return "Error while connecting to the database for updating."; }
+	// create a prepared statement
+	  String query = "UPDATE maintenance SET name=?,area=?,place=?,time=?,status=? WHERE maintenanceID=?";
+	  PreparedStatement preparedStmt = con.prepareStatement(query);
+	// binding values
+	   preparedStmt.setString(1, name);
+	   preparedStmt.setString(2, area);
+	   preparedStmt.setString(3, place);
+	   preparedStmt.setString(4, time);
+	   preparedStmt.setString(4, status);
+	   preparedStmt.setInt(5, Integer.parseInt(maintenanceID));
+	// execute the statement
+	   preparedStmt.execute();
+	   con.close();
+	   output = "Updated successfully";
+	}
+	catch (Exception e)
+	{
+	   output = "Error while updating the item.";
+	    System.err.println(e.getMessage());
+	}
+	   return output;
+ }
 
-		String output = "";
-			try
-			{
-				Connection con = connect();
-				if (con == null)
-				{return "Error while connecting to the database for updating."; }
-				// create a prepared statement
-				String query = "UPDATE MAintenance SET name=?, area=?,place=?,time=?, status=? where MaintenaceID=? ";
-						
-						
-				PreparedStatement preparedStmt = con.prepareStatement(query);
-				// binding values
-				preparedStmt.setString(1, name);
-				preparedStmt.setString(2, area);
-				preparedStmt.setString(3, place);
-				preparedStmt.setDouble(3, Double.parseDouble(time));
-				preparedStmt.setString(4, status);
-				preparedStmt.setInt(5, Integer.parseInt(MaintenanceID));
-				// execute the statement
-				preparedStmt.execute();
-				con.close();
-				output = "Updated successfully";
-			}
-		catch (Exception e)
-		{
-		output = "Error while updating the item.";
-		System.err.println(e.getMessage());
-		}
-		return output;
-		}	
-	
-	public String deleteMaintenance(String MaintenaceID)
-	{
-		String output = "";
-		try
-		{
-				Connection con = connect();
-				if (con == null)
-					{return "Error while connecting to the database for deleting."; }
-					// create a prepared statement
-					String query = "delete from items where MAintenanceID=?";
-					PreparedStatement preparedStmt = con.prepareStatement(query);
-					// binding values
-					preparedStmt.setInt(1, Integer.parseInt(MaintenaceID));
-					// execute the statement
-					preparedStmt.execute();
-					con.close();
-					output = "Deleted successfully";
-		}
-		catch (Exception e)
-		{
-			output = "Error while deleting the Maintenance.";
-			System.err.println(e.getMessage());
-		}
-		return output;
-	}
-	
-	
-	
-	
-	
+
 
 }
